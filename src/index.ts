545f9b42c7be026a0bc7816189d803b1b6898e33
otegami <a.s.takuya1026@gmail.com>
2022-06-01 08:25:37 +0900

    Implemented question

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,39 @@
-const message: string = "Hello, world"
-console.log(message)
+import { createInterface } from 'readline'
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('名前を入力してください:', (name) => {
+  console.log("こんにちは、" + name + "さん")
+  rl.close()
+})
+
+// const bigint = BigInt("fooooo")
+// console.log("bigint is ", bigint)
+
+// import { createInterface } from 'readline';
+
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
+
+// rl.question('文字列を入力してください:', (line) => {
+//   const num = Number(line)
+//   const result = num + 1000
+//   console.log(result)
+//   rl.close()
+// })
+
+
+
+// const message: string = "Hello, world"
+// console.log(message)
+
+// const greeting = "Hello";
+// const target = "world!";
+// console.log(greeting + target);
+
+// string, number, boolean, Bigint, null, undefined, symbol
