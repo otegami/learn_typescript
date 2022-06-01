54bd5deb2546af647e9696febf52f2170d18d2ac
otegami <a.s.takuya1026@gmail.com>
2022-06-02 08:07:32 +0900

    Relearn switch

--- src/episode2/index.ts
+++ src/episode2/index.ts
@@ -5,3 +5,17 @@ null == null
 const secret = process.env.SECRET ?? 'default'
 console.log(`secret は、 ${secret} です。`)
 
+const switcher = (name: string) => {
+  switch (name) {
+    case 'greet':
+      console.log('こんにちは')
+      break // <- これを忘れないで、無いと各 case を最後まで評価しちゃうよ
+    case 'cat':
+      console.log('あなたは猫派ですか？')
+      break
+    default:
+      console.log(`${name}は、外れ値です。`)
+  }
+}
+
+switcher('otegami')
