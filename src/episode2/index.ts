68b3be97e2821810ffa720f234b5832dd9b9b888
otegami <a.s.takuya1026@gmail.com>
2022-06-02 08:31:43 +0900

    learn fizzbuzz

--- src/episode2/index.ts
+++ src/episode2/index.ts
@@ -1,21 +1,45 @@
-// 下記の場合は true になるのでよく使われる
-null == undefined
-null == null
+// // 下記の場合は true になるのでよく使われる
+// null == undefined
+// null == null
 
-const secret = process.env.SECRET ?? 'default'
-console.log(`secret は、 ${secret} です。`)
+// const secret = process.env.SECRET ?? 'default'
+// console.log(`secret は、 ${secret} です。`)
 
-const switcher = (name: string) => {
-  switch (name) {
-    case 'greet':
-      console.log('こんにちは')
-      break // <- これを忘れないで、無いと各 case を最後まで評価しちゃうよ
-    case 'cat':
-      console.log('あなたは猫派ですか？')
-      break
-    default:
-      console.log(`${name}は、外れ値です。`)
+// const switcher = (name: string) => {
+//   switch (name) {
+//     case 'greet':
+//       console.log('こんにちは')
+//       break // <- これを忘れないで、無いと各 case を最後まで評価しちゃうよ
+//     case 'cat':
+//       console.log('あなたは猫派ですか？')
+//       break
+//     default:
+//       console.log(`${name}は、外れ値です。`)
+//   }
+// }
+
+// switcher('otegami')
+
+// let i = 1
+// while (i <= 100) {
+//   i++
+//   if (i % 2 === 1) {
+//     continue
+//   }
+//   console.log(i)
+// }
+
+const fizzBuzz = (max: number) => {
+  for (let i = 1; i <= max; i++) {
+    console.log(generateFizzbuzzNumber(i))
   }
 }
 
-switcher('otegami')
+const generateFizzbuzzNumber = (i: number): number | string => {
+  if (i % 15 === 0) return 'FizzBuzz'
+  if (i % 5 === 0) return 'Buzz'
+  if (i % 3 === 0) return 'Fizz'
+  return i
+}
+
+fizzBuzz(100)
