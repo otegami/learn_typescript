d7dd06cfd601181d5248e622e92c6dbb0c5c239e
otegami <a.s.takuya1026@gmail.com>
2022-07-21 07:42:01 +0900

    Finish to learn Episode7

--- src/episode7/fs.ts
+++ src/episode7/fs.ts
@@ -1,5 +1,32 @@
-import fs from 'fs'
+import path from "path"
+import { fileURLToPath } from "url"
+import { readFileSync } from "fs"
 
-const bufferedText = fs.readFileSync('/Users/otegami/Coding/typescript/practice/otegami.txt')
-const matchedOtegamiTimes = bufferedText.toString().match(/otegami/g)?.length ?? 0
-console.log(matchedOtegamiTimes)
+// // const bufferedText = fs.readFileSync('/Users/otegami/Coding/typescript/practice/otegami.txt')
+// // const matchedOtegamiTimes = bufferedText.toString().match(/otegami/g)?.length ?? 0
+// // console.log(matchedOtegamiTimes)
+
+// const data = readFileSync("otegami.txt", { encoding: "utf8" })
+// const matchedOtegamiTimes = data.match(/otegami/g)?.length ?? 0
+// console.log(matchedOtegamiTimes)
+
+const filename = fileURLToPath(import.meta.url)
+const fileDir = path.dirname(filename)
+const dataFIle = path.join(fileDir, "../../otegami.txt")
+// const otegamiFileURL = join(filename, '../../../', 'otegami.txt')
+console.log(dataFIle)
+const data = readFileSync(dataFIle, { encoding: "utf8" })
+
+let count = 0
+let currentIndex = 0
+
+while (true) {
+  const nextIndex = data.indexOf("otegami", currentIndex)
+  if (nextIndex >= 0) {
+    count++
+    currentIndex = nextIndex + 1
+  } else {
+    break
+  }
+}
+console.log(count)
