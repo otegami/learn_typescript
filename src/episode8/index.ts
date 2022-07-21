9e28faf635baa3e7718f7a0a5673e6935046f416
otegami <a.s.takuya1026@gmail.com>
2022-07-22 08:38:59 +0900

    learn how to create Promise Object

--- src/episode8/index.ts
+++ src/episode8/index.ts
@@ -1,5 +1,11 @@
 // import { readFile } from "fs"
 
+import { readFile } from "fs/promises";
+
+const sleepReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => { setTimeout(reject, duration) })
+}
+
 // // console.log("1. 読み込みを開始します")
 // // readFile("otegami.txt", (data) => {
 // //   console.log("3. 読み込みました")
@@ -28,16 +34,102 @@
 
 // Promise を使う
 
-import { readFile } from "fs/promises"
+// import { readFile } from "fs/promises"
 
 // success
-const p = readFile("otegami.txt", "utf8")
+// const p = readFile("otegami.txt", "utf8")
 // fail
 // const p = readFile("failed.txt", "utf8")
 
-p.then((data) => { console.log(data) })
-p.then((result) => { console.log("1") })
-p.then((result) => { console.log("3") })
-p.then((result) => { console.log("2") })
+// p.then((data) => { console.log(data) })
+// p.then((result) => { console.log("1") })
+// p.then((result) => { console.log("3") })
+// p.then((result) => { console.log("2") })
 
-p.catch((err) => { console.log("失敗", err) })
+// p.catch((err) => { console.log("失敗", err) })
+
+// p.then(
+//   (result) => {
+//     console.log("成功", result)
+//   },
+//   (error: unknown) => {
+//     console.log("失敗", error)
+//   })
+
+// const promise = new Promise<number>((resolve) => {
+//   setTimeout(() => {
+//     resolve(100)
+//   }, 3000)
+// })
+
+// promise.then((num) => { console.log(`結果は${num}`) })
+
+// const sleep = (duration: number) => {
+//   return new Promise<void>((resolve) => {
+//     setTimeout(resolve, duration)
+//   })
+// }
+
+// sleep(3000).then(() => { console.log("3秒経ちました") })
+
+// sleepReject(3000).catch(() => { console.log("失敗!!!") })
+
+// const p = Promise.resolve(100)
+// const instanceP = new Promise<number>((resolve) => { resolve(100) })
+
+// p.then(() => {
+//   console.log("2")
+// })
+// console.log("1")
+
+// // const r = Promise.reject('foo')
+// const instanceR = new Promise<never>((resolve, reject) => { reject('foo') })
+
+// instanceR.catch((err: string) => { console.log(err) })
+
+// const p = Promise.race([
+//   readFile("foo.txt", "utf8"),
+//   readFile("otegami.txt", "utf8"),
+//   readFile("foo.txt", "utf8"),
+// ])
+
+// p.then((result) => {
+//   console.log(result)
+// })
+
+// const p = Promise.race([
+//   readFile("foo.txt", "utf8"),
+//   sleepReject(5000)
+// ])
+
+// p.then((result) => {
+//   console.log("成功", result)
+// }, (error: unknown) => { console.log("失敗", error) })
+
+// const p = Promise.allSettled([
+//   readFile("foo.txt", "utf8"),
+//   sleepReject(500)
+// ])
+
+// p.then((result) => { console.log(result) })
+
+// // const p = readFile("otegami.txt", "utf8")
+// const p = readFile("error.txt", "utf8")
+// // const p2 = p.then((result) => result + result)
+// const p2 = p.catch(() => "")
+
+// p2.then((result) => {
+//   console.log(result)
+// })
+
+// const repeat10 = (str: string) => new Promise<string>((resolve) => { setTimeout(() => resolve(str.repeat(10)), 1000) })
+// const p1 = readFile("otegami.txt", "utf8")
+// const p2 = p1.then((result) => repeat10(result))
+// const p3 = p2.then((result) => { console.log(result) })
+// console.log('1')
+
+const p1 = readFile("otegami", "utf8")
+const p2 = p1.then((result) => {
+  throw new Error("Error!!!")
+})
+p2.then((result) => { console.log(result) })
